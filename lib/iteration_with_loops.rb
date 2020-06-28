714f6f2759ff3e0eee2d9fc77b3954562dc670fb
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-06-28 02:45:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def join_nested_strings(src)
  while index < src.count do
    element_index = 0 
    while element_index < src[index].count do
-     if src
+     if src[index][element_index] == String
      mix_data == src[index][element_index]
      element_index += 1
    end
